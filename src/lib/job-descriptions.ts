10e556903c3087ed7278c3e069768fcb9ac7655e
bollokakos-galactic <akos.bollok@galactic-fleet.com>
2025-07-10 11:07:39 +0200

    added software engineer role

--- src/lib/job-descriptions.ts
+++ src/lib/job-descriptions.ts
@@ -42,6 +42,42 @@ export const jobs: Array<JobDescription> = [
 		}
 	},
 	{
+		title: 'SOFTWARE ENGINEER',
+		description:
+			'You will deal with the interaction model, persisting user data, create and orchestrate experiments with extensive logging, build visual and vocal interactions, deal with issues reported by actual players.',
+		slug: 'software-engineer',
+		whatYouDo: [
+			'Implement engineering solutions for new game features or experiments',
+			'Deliver your work in time',
+			'Identify roadblocks and raise awareness',
+			'Work with product managers, designers and QA people to understand the UX we are aiming for',
+			'End-to-end ownership of games from the backend and UI flows to the tested and released version',
+			'Make your work trackable and visible by reporting to stakeholders',
+			'Create maintainable code, no matter if it is new or a refactored old one',
+		],
+		whatWeLookFor: [
+			'Strong computer science fundamentals, including knowledge of data structures and types, asynchronous algorithms, clean code, OOP and functional programming',
+			'OK to write throw-away code and experiment with UX',
+			'1+ years of experience building APIs, web services and frontend applications',
+			'3+ years of experience with TypeScript and Node.js',
+			'Experience working with SQL and/or NoSQL databases (DynamoDB, MongoDB, etc…)',
+			'You are able to learn quickly and get up to speed',
+			'You speak English with confidence - we are working with US studios'
+
+		],
+		whatWeOffer: [
+		'Deep insights into the world of video games - work with professional US studios',
+		'Opportunity to work independently and creatively - always appreciate your ideas and proactivity',
+		'Flexible, hybrid working arrangements - we aim for the right work-life balance',
+		'Cool office environment in the city centre of Budapest - relaxation facilities, office massage, free networking events',
+		'Transparent, open and friendly company culture - everyone from various backgrounds, experiences, and perspectives are valued and respected',
+		],
+		howToApply: {
+			text: "Please apply with an English CV in email: ",
+			contact: "laura.kocsis@galactic-fleet.com"
+		}
+	},
+	{
 		title: 'MARKETING INTERN',
 		description:
 			"In  this  role,  you  will  be  responsible  for creating strategies for reaching our target audience and building our community. Your role will also include managing our social media platforms and  creating  content  that  represents  our  studio's culture and helps people connect with us and our games.",
